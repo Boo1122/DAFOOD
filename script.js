25cb74636926d5c3fc38eeb423bdd71bd705abe7
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-03-01 18:38:45 +0200

    j

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function HomeButton(){
     })
     }
   if (location.href == "http://127.0.0.1:5501/MealsByCountry.html"){
-    generateFlagPage();~
+    generateFlagPage();
   }
 
     function generateFlagPage(){
