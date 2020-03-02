ee41908d4948fa0dc6f3e8c3e5faf9208488146b
Boo1122 <bbb_slv@hotmail.co.uk>
2020-03-02 12:47:48 +0200

    input

--- searchButton.js
+++ searchButton.js
@@ -1,19 +1,8 @@
 
+let searchInput = document.getElementById("search");
 
-let search = document.getElementById("search").addEventListener("click", function(event){
-    if(event.key === "Enter") {
-        const searchValue = event.target.value;
-
-const menu = document.getElementsById("menu")
-    for(let elements of menu) {
-        const textMenu = elements.innerText;
-        // const menuId = menu.parentNode.parentNode.parentNode.id;
-
-    if(textMenu.includes(searchValue)) {
-        console.log("includes")
-             }        
-        }
-    }
+searchInput.addEventListener('input', () => {
+    console.log('input was fired')
 })
 
-
+    
