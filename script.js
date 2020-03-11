dddd16ee9bfeb1f84e94045b9661ac2f4ea8d979
Boo1122 <you@example.com>
2020-03-11 09:15:31 +0200

    asda

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function HomeButton(){
   document.getElementById("home").addEventListener("click", function(){
     console.log("Lol");
-    window.location.href = "./index.html";
+    window.location.href = "/MealsByCountry.html";
   })
   }
 if (location.href == "./Meals by country.html"){
@@ -41,56 +41,7 @@ if (location.href == "./Meals by country.html"){
       flagContainer.appendChild(img); 
       img.style.margin = "20px";
     }
-  if (location.href == "/MealsByCountry.html"){
-    generateFlagPage();
-  }
-
-    function generateFlagPage(){
-      const flagPage = document.createElement("div");
-      flagPage.classList.add("flag-page");
-      flagPage.id = "flag-page";
-      document.body.appendChild(flagPage);
-
-      addExplantion(flagPage);
-      generateFlags(flagPage);
-    }
-
-    function addExplantion(flagPage){
-      const explanation = document.createElement("h2");
-      explanation.classList.add("explanation");
-      explanation.innerText = "Choose a flag";
-      explanation.style.textAlign = "center";
-      flagPage.appendChild(explanation);
-    }
-
-    function generateFlags(flagPage){
-
-      const flagShortName = ["fr.png", "gb.png", "us.png"];
-      const flagContainer = document.createElement('div');
-      flagContainer.classList.add("flag-Container");
-      flagContainer.style.textAlign = "center";
-      flagPage.appendChild(flagContainer);
-
-      for (let index = 0; index < flagShortName.length; index++) {
-        const img = document.createElement("img");
-        img.id="flags"
-        img.src=flagShortName[index];
-        flagContainer.appendChild(img); 
-        img.style.margin = "20px";
-      }
-    }
-
-    function clickOnFlag(){
-      document.getElementById("flags").addEventListener("click", generateMealPage)
-      
-    }
-    function generateMealPage(event) {
-      console.log("generating meal page");
-      highlightMealButton();
-      hideHomePage();
-      getMealsFromServer(event.target.innerText);
-    }
-    
+ 
   HomeButton();
   }
 
