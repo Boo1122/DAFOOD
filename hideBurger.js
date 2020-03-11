69cb2db846144306d8bc5a6e4e8eb740ac51f134
Boo1122 <you@example.com>
2020-03-11 08:40:14 +0200

    asd

--- hideBurger.js
+++
@@ -1,6 +0,0 @@
-function hide() {
-    let menu = document.getElementById('menu')
-    flyout.style.display = "none"
-}
-
-input.addEventListener("blur", hide)
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function HomeButton(){
     window.location.href = "./index.html";
   })
   }
-if (location.href == "http://127.0.0.1:5501/./Meals by country.html"){
+if (location.href == "./Meals by country.html"){
   generateFlagPage();
 }
 
@@ -41,58 +41,7 @@ if (location.href == "http://127.0.0.1:5501/./Meals by country.html"){
       flagContainer.appendChild(img); 
       img.style.margin = "20px";
     }
-  if (location.href == "http://127.0.0.1:5501/MealsByCountry.html"){
-    generateFlagPage();
-  }
-
-    function generateFlagPage(){
-      const flagPage = document.createElement("div");
-      flagPage.classList.add("flag-page");
-      flagPage.id = "flag-page";
-      document.body.appendChild(flagPage);
-
-      addExplantion(flagPage);
-      generateFlags(flagPage);
-    }
-
-    function addExplantion(flagPage){
-      const explanation = document.createElement("h2");
-      explanation.classList.add("explanation");
-      explanation.innerText = "Choose a flag";
-      explanation.style.textAlign = "center";
-      flagPage.appendChild(explanation);
-    }
-
-    function generateFlags(flagPage){
-
-      const flagShortName = ["fr.png", "gb.png", "us.png"];
-      const flagContainer = document.createElement('div');
-      flagContainer.classList.add("flag-Container");
-      flagContainer.style.textAlign = "center";
-      flagPage.appendChild(flagContainer);
-
-      for (let index = 0; index < flagShortName.length; index++) {
-        const img = document.createElement("img");
-        img.id="flags"
-        img.src=flagShortName[index];
-        flagContainer.appendChild(img); 
-        img.style.margin = "20px";
-      }
-    }
-
-    function clickOnFlag(){
-      document.getElementById("flags").addEventListener("click", generateMealPage)
-      
-    }
-    function generateMealPage(event) {
-      console.log("generating meal page");
-      highlightMealButton();
-      hideHomePage();
-      getMealsFromServer(event.target.innerText);
-    }
-    
-  HomeButton();
-  }
+ 
 
   function clickOnFlag(){
     document.getElementById("flags").addEventListener("click", generateMealPage)
@@ -104,5 +53,6 @@ if (location.href == "http://127.0.0.1:5501/./Meals by country.html"){
     hideHomePage();
     getMealsFromServer(event.target.innerText);
   }
+}
   
 HomeButton();
